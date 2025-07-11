0f2e38e525a735e7997656d72fa5f189ac36549a
JoksLobo <jocson_herry@hotmail.com>
2025-07-11 14:20:45 -0300

    adição de carrossel, seção loja

---
+++ script.js
@@ -0,0 +1,19 @@
+// let currentIndex = 0;
+
+// function updateCarousel() {
+//   const track = document.getElementById("track");
+//   track.style.transform = `translateX(-${3000 * currentIndex}px)`;
+// }
+
+// function prevSlide() {
+//   if (currentIndex > 0) {
+//     currentIndex--;
+//     updateCarousel();
+//   }
+// }
+// function nextSlide() {
+//   if (currentIndex < 2) {
+//     currentIndex++;
+//     updateCarousel();
+//   }
+// }
